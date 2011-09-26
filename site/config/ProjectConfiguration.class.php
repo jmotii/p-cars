6d68724b50460c512553f74d8f0a0d722d836087
Motii Jihad <motiijihad@gmail.com>
2011-09-26 16:34:56 +0000

    Testing commit from netbeans work

--- site/config/ProjectConfiguration.class.php
+++ site/config/ProjectConfiguration.class.php
@@ -3,7 +3,6 @@
 require_once dirname(__FILE__).'/../lib/vendor/symfony/lib/autoload/sfCoreAutoload.class.php';
 sfCoreAutoload::register();
 
-
 class ProjectConfiguration extends sfProjectConfiguration
 {
   public function setup()
